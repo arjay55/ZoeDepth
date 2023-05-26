76a9008d31ea909a1f5bac9e76d1c1dfe5843a6e
arjay55 <killer2ma2@yahoo.com>
2023-05-27 06:30:57 +0800

    simplify first

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
     ],
-    package_data={'zoedepth': ['models/zoedepth/*.json', 'models/zoedepth_nk/*.json'],
+    package_data={'zoedepth': ['models/zoedepth/*.json'],
                 },
 )
