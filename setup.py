f17b0bffa3daee291d4ee59cca911bd816c8ff2b
arjay55 <killer2ma2@yahoo.com>
2023-05-26 17:06:36 +0800

    include zoedepth

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version="0.5",
     author="Bhat, Shariq Farooq and Birkl, Reiner and Wofk, Diana and Wonka, Peter and Müller, Matthias",
     description="A short description of your package",
-    packages=setuptools.find_packages(exclude=['zoedepth']),
+    packages=setuptools.find_packages(include=['zoedepth']),
     install_requires=[
         ],
 )
