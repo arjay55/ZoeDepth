3da1f0d4973329fa039d33b05e582b6b0dde644c
arjay55 <killer2ma2@yahoo.com>
2023-05-26 18:56:04 +0800

    try again on versioning

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 import setuptools
+from codecs import open
+
+exec(open('timm/version.py').read())
 
 setuptools.setup(
     name="zoedepth",
-    version="0.5",
+    version=__version__,
     author="Bhat, Shariq Farooq and Birkl, Reiner and Wofk, Diana and Wonka, Peter and Müller, Matthias",
     description="A short description of your package",
     packages=setuptools.find_packages(),
