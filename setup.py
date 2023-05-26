fa5079f329ebf1ff4cad917be8f2fbe797864629
Arjeus Guevarra <killer2ma2@yahoo.com>
2023-05-26 16:36:07 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="zoedepth",
+    version="0.5",
+    author="Bhat, Shariq Farooq and Birkl, Reiner and Wofk, Diana and Wonka, Peter and Müller, Matthias",
+    description="A short description of your package",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        ],
+)
